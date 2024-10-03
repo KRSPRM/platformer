7aa145bb3e50fbe6083a65ca3b2a2840df5792bc
KRSPRM <sirovykry@gmail.com>
2024-10-03 16:53:42 +0200

    Camera movement and clouds

--- game.py
+++ game.py
@@ -4,12 +4,13 @@ import pygame
 from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 class Game:
     def __init__(self):
         pygame.init()
         
-        pygame.display.set_caption('DEJV SIMULATOR')
+        pygame.display.set_caption('PLATFORMER')
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
 
@@ -22,22 +23,36 @@ class Game:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png')
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds'),
         }
+        
+        self.clouds = Clouds(self.assets['clouds'], count = 16)
         
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
         
         self.tilemap = Tilemap(self, tile_size = 16)
         
+        # Camera movement, we are basically moving everything rendered on the screen
+        self.scroll = [0, 0]
+        
         
     def run(self):
         while True:
-            self.display.fill((0, 150, 255))
+            self.display.blit(self.assets['background'], (0, 0))
             
-            self.tilemap.render(self.display)
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+            
+            self.clouds.update()
+            self.clouds.render(self.display, offset = render_scroll)
+            
+            self.tilemap.render(self.display, offset = render_scroll)
             
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0)) # You only run left or right. We don't need Y-axis
-            self.player.render(self.display)
+            self.player.render(self.display, offset = render_scroll)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
