7be695dc5c7b68eb68df2c7dd07cd6358659dfb7
KRSPRM <sirovykry@gmail.com>
2024-10-19 13:53:07 +0200

    Jump and wall jump

--- scripts/entities.py
+++ scripts/entities.py
@@ -14,6 +14,8 @@ class PhysicsEntity:
         self.anim_offset = (-3, -3)
         self.flip = False
         self.set_action('idle')
+        
+        self.last_movement = [0, 0]
 
 
     def rect(self) -> None:
@@ -59,8 +61,10 @@ class PhysicsEntity:
             self.flip = False
         if movement[0] < 0:
             self.flip = True
+            
+        self.last_movement = movement
         
-        self.velocity[1] = min(5, self.velocity[1] + 0.1)
+        self.velocity[1] = min(5, self.velocity[1] + 0.1) # Normalization for vertical velocity
         
         if self.collisions['down'] or self.collisions['up']:
             self.velocity[1] = 0
@@ -78,6 +82,8 @@ class Player(PhysicsEntity):
     def __init__(self, game, pos, size) -> None:
         super().__init__(game, 'player', pos, size)
         self.air_time = 0
+        self.jumps = 1
+        self.wall_slide = False
 
 
     def update(self, tilemap, movement=(0, 0)) -> None:
@@ -86,12 +92,50 @@ class Player(PhysicsEntity):
         self.air_time += 1
         if self.collisions['down']:
             self.air_time = 0
+            self.jumps = 1
         
-        if self.air_time > 4:
-            self.set_action('jump')
-        elif movement[0] != 0:
-            self.set_action('run')
+        self.wall_slide = False
+        if (self.collisions['right'] or self.collisions['left']) and self.air_time > 4:
+            self.wall_slide = True
+            self.velocity[1] = min(self.velocity[1], 0.5)
+            if self.collisions['right']:
+                self.flip = False
+            else:
+                self.flip = True
+            self.set_action('wall_slide')
+        
+        if not self.wall_slide: 
+            if self.air_time > 4:
+                self.set_action('jump')
+            elif movement[0] != 0:
+                self.set_action('run')
+            else:
+                self.set_action('idle')
+                
+        if self.velocity[0] > 0: # Normalization for horizontal velocity
+            self.velocity[0] = max(self.velocity[0] - 0.1, 0)
         else:
-            self.set_action('idle')
-        
+            self.velocity[0] = min(self.velocity[0] + 0.1, 0)
+
+    
+    def jump(self) -> None:
+        if self.wall_slide:
+            if self.flip and self.last_movement[0] < 0:
+                self.velocity[0] = 3.5
+                self.velocity[1] = -2.5
+                self.air_time = 5
+                self.jumps = max(0, self.jumps - 1)
+                return True
+            elif not self.flip and self.last_movement[0] > 0:
+                self.velocity[0] = -3.5
+                self.velocity[1] = -2.5
+                self.air_time = 5
+                self.jumps = max(0, self.jumps - 1)
+                return True
+            
+        elif self.jumps:
+            self.velocity[1] = -3
+            self.jumps -= 1
+            self.air_time = 5
+            return True
         
